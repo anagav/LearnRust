6d07df57bb16d882770a84e782682086156caadb
ashishn <nagav.ashish@gmail.com>
2015-08-02 20:15:22 -0700

    changes checked in

--- median_sorted_arrays/src/max_product/mod.rs
+++ median_sorted_arrays/src/max_product/mod.rs
@@ -5,7 +5,7 @@ fn max_product_subarray(stack: &mut Vec<i32>)  {
 	let mut cur_max = 1;
 	let mut max = 0;
 	for element in stack {
-		cur_max = std::cmp::max(1, cur_max * (*element));
+		cur_max = std::cmp::max(*element, cur_max * (*element));
 		max = std::cmp::max(cur_max, max);
 	}
 	println!("{:?}", max);
@@ -13,6 +13,6 @@ fn max_product_subarray(stack: &mut Vec<i32>)  {
 
 
 pub fn run() {
-		let mut stack:Vec<i32> = vec![1,2,3,-4,5,-6];
+		let mut stack:Vec<i32> = vec![1,2,3,-4,5,-6,10];
 		max_product_subarray(&mut stack);
 }
